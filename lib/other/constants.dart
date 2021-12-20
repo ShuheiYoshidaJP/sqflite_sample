13937e5778acb791e44c05a2cca1c73a2a5bd296
Shuhei Yoshida <aneki525@gmail.com>
2021-12-20 12:59:24 +0900

    update constants

--- lib/other/constants.dart
+++ lib/other/constants.dart
@@ -2,4 +2,8 @@ class Constants {
   // text
   static String save = 'save';
   static String searchCustomer = 'Search Customer Name';
+  static String caution = 'Caution';
+  static String deleteCustomer = 'Delete Customer';
+  static String cancel = 'Cancel';
+  static String ok = 'OK';
 }
